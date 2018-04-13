d0cd5bf41658094df5171e196b18916654c96eb2
Hider <dtwdevelop@gmail.com>
2018-04-13 18:05:53 +0100

    add components

--- pacelam/src/app/app.module.ts
+++ pacelam/src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ClientComponent } from './client/client.component';
+import { PaymentComponent } from './payment/payment.component';
+import { StatusDirective } from './status.directive';
+import { MapComponent } from './map/map.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientComponent,
+    PaymentComponent,
+    StatusDirective,
+    MapComponent
   ],
   imports: [
     BrowserModule
---
+++ pacelam/src/app/status.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appStatus]'
+})
+export class StatusDirective {
+
+  constructor() { }
+
+}
